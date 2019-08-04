511d4b528205e819f8a06f328d6710ceda62533c
beepscore <support@beepscore.com>
2019-08-03 20:39:20 -0700

    extract methods. add unit tests. rename example data files

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from io import StringIO
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
@@ -12,10 +11,24 @@ reference websearcher browser_driver.py
 https://github.com/beepscore/websearcher
 """
 
+# css id used to select html containing table within page html. Includes a little additional html
+css_id = 'financials-iframe-wrap'
 
-def url(stock_symbol):
+
+def get_html_filename(stock_symbol):
     """
-    return string representing url
+    :param stock_symbol: used to construct the html data filename, e.g. 'nflx'
+    :return: string representing filename
+    e.g. './data/<stock_symbol>.html', './data/nflx.html'
+    """
+    path = './data/' + stock_symbol + '.html'
+    return path
+
+
+def get_url(stock_symbol):
+    """
+    :param stock_symbol: e.g. 'nflx'
+    :return: string representing url
     e.g. 'https://www.nasdaq.com/symbol/nflx/financials'
     """
     base_url = 'https://www.nasdaq.com'
@@ -25,7 +38,7 @@ def url(stock_symbol):
     return url_string
 
 
-def get_html(url, css_id):
+def get_html_from_web(stock_symbol):
     """
     Uses browser to request info.
     Waits for javascript to run and return html. Selects by css_id.
@@ -36,6 +49,7 @@ def get_html(url, css_id):
 
     browser = webdriver.Chrome()
 
+    url = get_url(stock_symbol)
     browser.get(url)
 
     try:
@@ -65,45 +79,55 @@ def get_html(url, css_id):
         browser.quit()
 
 
-def get_dataframe(url, css_id):
+def get_dataframe_from_file(filename):
     """
-    :param url: url to load
-    :param css_id: id of element to select
-    :return: dataframe
+    # reads data from local file. This can be handy during development.
+    :param filename: e.g. './data/<stock_symbol>.html', './data/nflx.html'
+    :return: a pandas dataframe
     """
 
-    # to read from local data file, uncomment these.
-    # this can be handy during development
-    # dataframes = pd.read_html('./data/html.html', header=0, index_col=0, skiprows=0)
+    dataframes = pd.read_html(filename, header=0, index_col=0, skiprows=0)
 
-    # to read from web, uncomment these
-    html = get_html(url, css_id)
-    print('html:\n', html)
+    # read_html returns a list of dataframes, get the first one
+    df = dataframes[0]
+
+    df = cleaned_df(df)
+    return df
+
+
+def get_dataframe_from_web(stock_symbol):
+    """
+    constructs a url from stock_symbol, downloads web page, returns dataframe
+    :param stock_symbol: e.g. 'nflx'
+    :return: a pandas dataframe
+    """
+
+    url = get_url(stock_symbol)
+    html = get_html_from_web(url)
     dataframes = pd.read_html(html, header=0, index_col=0, skiprows=0)
 
     # read_html returns a list of dataframes, get the first one
     df = dataframes[0]
 
     df = cleaned_df(df)
-
     return df
 
 
 def cleaned_df(df):
     """
-    :param df: dataframe from read_html
+    :param df: dataframe of the form returned by get_dataframe_from_web or get_dataframe_from_file
     :return: dataframe with 4 columns containing amounts in thousands of dollars $000
     """
     # drop rows with all values NaN (Not A Number)
     df = df.dropna(how='all')
 
     # print(df.columns.values)
-    # get desired column names. The values may change over time.
+    # get desired column names. Note the dates may change over time.
     column_names = df.columns.values[1:5]
-    print(column_names)
+    # print(column_names)
     # ['12/31/2018' '12/31/2017' '12/31/2016' '12/31/2015']
 
-    # the dollar amounts are in columns "Unnamed:25 through Unnamed:28
+    # the dollar amounts are in columns Unnamed:25 through Unnamed:28
     # slice to keep columns with dollar amounts
     df = df.loc[:, "Unnamed: 25": 'Unnamed: 28']
     # re-add column names
@@ -112,21 +136,56 @@ def cleaned_df(df):
     return df
 
 
+def get_revenue(df):
+    """
+    :param df:
+    :return: float
+    """
+    # pandas loc slice [:, 0:1] gets all rows, columns 0 through 1 inclusive
+    newest_year_df = df.iloc[:, 0:1]
+    revenue_dollar_string = newest_year_df.loc['Total Revenue', :].values[0]
+    revenue = dollars(revenue_dollar_string)
+    return revenue
+
+
+def dollars(dollar_string):
+    """
+    :param dollar_string: a string representing dollars () for negative amount e.g. $(100.21)
+    :return: a float e.g. -100.21
+    """
+
+    if "(" in dollar_string:
+        # prepend minus sign
+        dollar_string = "-" + dollar_string
+
+    # remove any parentheses
+    dollar_string = dollar_string.replace('(', '')
+    dollar_string = dollar_string.replace(')', '')
+
+    # remove dollar sign
+    dollar_string = dollar_string.replace('$', '')
+    # remove comma
+    dollar_string = dollar_string.replace(',', '')
+
+    amount = float(dollar_string)
+    return amount
+
+
 if __name__ == '__main__':
 
     stock_symbol = 'nflx'
-    url = url(stock_symbol)
-    print(url)
 
-    css_id = 'financials-iframe-wrap'
+    filename = get_html_filename(stock_symbol)
+    df = get_dataframe_from_file(filename)
 
-    df = get_dataframe(url, css_id)
-    # write dataframe to a csv file
-    df.to_csv('./data/data.csv')
+    # alternatively
+    # df = get_dataframe_from_web(stock_symbol)
 
     print(df)
 
-    # pandas loc slice [:, 0:1] gets all rows, columns 0 through 1 inclusive
-    newest_year_df = df.iloc[:, 0:1]
-    revenue = newest_year_df.loc['Total Revenue', :].values[0]
+    # write dataframe to a csv file
+    # many programs can read this format e.g. pandas, excel
+    df.to_csv('./data/' + stock_symbol + '.csv')
+
+    revenue = get_revenue(df)
     print(revenue)
---
+++ tests/scrapper_tests.py
@@ -0,0 +1,33 @@
+import unittest
+import scrapper
+
+
+class ScrapperTests(unittest.TestCase):
+
+    def test_get_html_filename(self):
+        self.assertEqual(scrapper.get_html_filename('nflx'), './data/nflx.html')
+        self.assertEqual(scrapper.get_html_filename('appl'), './data/appl.html')
+
+    def test_get_url(self):
+        self.assertEqual(scrapper.get_url('nflx'), 'https://www.nasdaq.com/symbol/nflx/financials')
+
+    def test_get_dataframe_from_file(self):
+        df = scrapper.get_dataframe_from_file('../data/nflx.html')
+        # 18 rows x 4 columns
+        self.assertEqual(df.shape, (18, 4))
+
+    def test_dollars(self):
+        # use assertAlmostEqual instead of assertEqual to allow for float tolerance
+        self.assertAlmostEqual(scrapper.dollars('$123.45'), 123.45)
+        self.assertAlmostEqual(scrapper.dollars('$(123.45)'), -123.45)
+
+        self.assertAlmostEqual(scrapper.dollars('$123,456,789)'), 123456789)
+
+    def test_get_revenue(self):
+        df = scrapper.get_dataframe_from_file('../data/nflx.html')
+        revenue = scrapper.get_revenue(df)
+        self.assertAlmostEqual(revenue, 15794341.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
