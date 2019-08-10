05d56c09597d01f7d677b19335b981f514fa6675
beepscore <support@beepscore.com>
2019-08-09 21:30:53 -0700

    add assertions

--- tests/filename_constructor_tests.py
+++ tests/filename_constructor_tests.py
@@ -6,7 +6,14 @@ from pathlib import Path
 class FilenameConstructorTests(unittest.TestCase):
 
     def test_get_balance_sheet_csv_filename(self):
-        self.assertEqual(filename_constructor.get_balance_sheet_csv_filename('nflx'), Path('./data/nflx_balance_sheet.csv'))
+        path = filename_constructor.get_balance_sheet_csv_filename('nflx')
+        self.assertEqual(path, Path('./data/nflx_balance_sheet.csv'))
+        self.assertEqual(path.parent, Path('data'))
+        self.assertEqual(path.anchor, '')
+        self.assertEqual(path.name, 'nflx_balance_sheet.csv')
+        self.assertEqual(path.stem, 'nflx_balance_sheet')
+        self.assertEqual(path.suffix, '.csv')
+
         self.assertEqual(filename_constructor.get_balance_sheet_csv_filename('appl'), Path('./data/appl_balance_sheet.csv'))
 
     def test_get_income_csv_filename(self):
